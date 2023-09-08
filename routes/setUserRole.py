2dd4bbe2e4aeb41c9fe0ce34923d4a6cd6ec2349
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-09-08 20:18:48 +0200

    Update setUserRole.py

--- routes/setUserRole.py
+++ routes/setUserRole.py
@@ -12,13 +12,12 @@ def setUserRole(userName,newRole):
                 f'select role from users where userName = "{session["userName"]}"'
             )
             role = cursor.fetchone()[0]
-            match role == "admin":
-                case  True:
+            if role == "admin":
                     cursor.execute(f'update users set role = "{newRole}" where lower(userName) = "{userName.lower()}" ')
                     connection.commit()
                     message("2",f'ADMIN: "{session["userName"]}" CHANGED USER: "{userName}"s ROLE TO {newRole}')
                     return redirect("/admin/users")
-                case False:
+            else:
                      return redirect("/")    
     else:
             return redirect("/")    
