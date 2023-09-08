d73fc51ed779d5dc5324a7d8c1f752d8d1651ee6
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-09-08 18:49:46 +0200

    Update accountSettings.py

--- routes/accountSettings.py
+++ routes/accountSettings.py
@@ -13,8 +13,7 @@ accountSettingsBlueprint = Blueprint("accountSettings", __name__)
 
 @accountSettingsBlueprint.route("/accountsettings", methods=["GET", "POST"])
 def accountSettings():
-    match "userName" in session:
-        case True:
+    if "userName" in session:
             connection = sqlite3.connect("db/users.db")
             cursor = connection.cursor()
             cursor.execute(
@@ -26,5 +25,5 @@ def accountSettings():
                     deleteUser(user[0][0])
                     return redirect(f"/")
             return render_template("accountSettings.html", user=user)
-        case False:
+    else:
             return redirect("/login/redirect=&accountsettings")
